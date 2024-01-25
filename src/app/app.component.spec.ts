7c5eedaa1e2c11fa052a0d88fd5544073b14acbc
Caio Braga <caiotabraga@hotmail.com>
2024-01-25 11:17:10 -0300

    starting project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'AngularProject'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AngularProject');
+    expect(app.name).toEqual('AngularProject');
   });
 
   it('should render title', () => {
